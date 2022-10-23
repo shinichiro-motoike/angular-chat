6a4d09b86b368d87629a4b708dd3bcacb361e082
motoike <motoike.shi@gmail.com>
2022-10-23 17:23:39 +0900

    add comment functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+// モックデータ
+const COMENTS: Comment[] = [
+  { name: 'Kenzi', message: 'こんにちは' },
+  { name: 'Kenzi', message: 'さようなら' },
+  { name: 'Mitsiko', message: 'また会う日まで！' },
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMENTS;
 }
---
+++ src/app/class/comment.ts
@@ -0,0 +1,4 @@
+export class Comment {
+  name: string | undefined;
+  message!: string;
+}
